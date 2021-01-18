75bb3a4f6282cf97c3a7688e1f68a08f85d18bf5
John Schroeder <4474856+johnlovesdata@users.noreply.github.com>
2021-01-17 19:10:14 -0600

    update account info add profile pic

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
+import secrets
+import pathlib
+from PIL import Image
 
 posts = [
     {
@@ -72,7 +75,38 @@ def logout():
     return redirect(url_for("home"))
 
 
-@app.route("/account")
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    file_ext = pathlib.Path(form_picture.filename).suffix
+    image_filename = random_hex + file_ext
+    picture_path = pathlib.Path(app.root_path).joinpath(
+        "static", "profile_pics", image_filename
+    )
+    output_size = (128, 128)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return image_filename
+
+
+@app.route("/account", methods=["GET", "POST"])
 @login_required
 def account():
-    return render_template("account.html", title="Account")
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("Your account has been updated.", "success")
+        return redirect(url_for("account"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_filename = f"profile_pics/{current_user.image_file}"
+    image_file = url_for("static", filename=image_filename)
+    return render_template(
+        "account.html", title="Account", image_file=image_file, form=form
+    )
